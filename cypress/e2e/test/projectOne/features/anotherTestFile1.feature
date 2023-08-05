7533b0f581a5ced5dc8a4096f41fe12bb33252d0
Patrick O <patrick.obiaso@ofsted.gov.uk>
2023-08-05 10:14:37 +0100

    Added a dummy feature file and step definition

--- cypress/e2e/test/projectOne/features/anotherTestFile1.feature
+++ cypress/e2e/test/projectOne/features/anotherTestFile1.feature
@@ -1,2 +1,7 @@
 Feature: featureName
   what the feature is testing
+
+  Scenario: Verify that user can use the google search engine
+    Given I navigate to google homepage
+    When I Search for the word "BBC news"
+    Then The search result should include "BBC news"
--- cypress/e2e/test/projectOne/stepDefinitions/anotherTestFile1-steps.js
+++ cypress/e2e/test/projectOne/stepDefinitions/anotherTestFile1-steps.js
@@ -1,5 +1,17 @@
 import { When, Given, Then } from '@badeball/cypress-cucumber-preprocessor'
 
-When(/^I open the "([^"]*)" "([^"]*)" task in portal$/, function (taskStatus, taskName) {
-
+When(/^I navigate to google homepage$/, function (taskStatus, taskName) {
+  cy.visit('/')
 })
+
+When(/^I Search for the word "([^"]*)"$/, function (searchWord) {
+   cy.get('#search').type(searchWord)
+   cy.get('.searchIcon').click()
+})
+
+When(/^The search result should include "([^"]*)"$/, function (searchWord) {
+   cy.get('div').contains(searchWord).should('be.visible')
+})
+
+// FYI : the locators are a sample dummy but it gives the illustration on how to locate element in
+// Cypress
